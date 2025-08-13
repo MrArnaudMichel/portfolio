8f9d40d44e90472dadd6e9ce0afe5a4bf2f69563
Arnaud Michel <contact@arnaudmichel.fr>
2025-08-13 20:02:59 +0200

    Add changelogVersions UI configuration and update slot classes in app config

--- app/app.config.ts
+++ app/app.config.ts
@@ -4,5 +4,15 @@ export default defineAppConfig({
       primary: 'green',
       neutral: 'slate'
     }
+  },
+  uiPro: {
+    changelogVersions: {
+      slots: {
+        root: 'relative',
+        container: 'flex flex-col gap-y-8 sm:gap-y-12 lg:gap-y-16',
+        indicator: 'absolute hidden lg:block overflow-hidden inset-y-3 left-32 h-full w-px bg-green-400 dark:bg-green-600 -ml-[8.5px]',
+        beam: 'absolute left-0 top-0 w-full bg-primary will-change-[height]'
+      }
+    }
   }
 })
