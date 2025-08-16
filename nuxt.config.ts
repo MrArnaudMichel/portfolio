8b795ab76a77cb5465554b5d778aea879dc23e34
Arnaud Michel <contact@arnaudmichel.fr>
2025-08-16 21:22:06 +0200

    Rename "scholarship" to "schooling" in content structure and update related configuration and routes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,8 +19,8 @@ export default defineNuxtConfig(
     css: ['~/assets/css/main.css'],
 
     routeRules: {
-      '/docs': {
-        redirect: '/docs/getting-started',
+      '/career': {
+        redirect: '/career/schooling',
         prerender: false
       }
     },
